225efaeba61a5240fb7b1afbc224f6712d1c5029
chulkov-alex <chulkov-alex@yandex-team.ru>
2024-12-31 20:30:45 +0300

    migrate onto polemica library

--- src/main/kotlin/com/github/mafia/vyasma/polemicaachivementservice/crawler/CrawlerConfiguration.kt
+++ src/main/kotlin/com/github/mafia/vyasma/polemicaachivementservice/crawler/CrawlerConfiguration.kt
@@ -5,23 +5,24 @@ import com.github.mafia.vyasma.polemica.library.client.PolemicaClientImpl
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class CrawlerConfiguration {
-    @Value("\${bot.api.baseUrl:https://app.polemicagame.com}")
-    private lateinit var botBaseUrl: String
+    @Value("\${polemica.api.baseUrl:https://app.polemicagame.com}")
+    private lateinit var polemicaBaseUrl: String
 
-    @Value("\${POLEMICA_TOKEN}")
-    private lateinit var botToken: String
+    @Value("\${POLEMICA_USERNAME}")
+    private lateinit var polemicaUsername: String
+
+    @Value("\${POLEMICA_PASSWORD}")
+    private lateinit var polemicaPassword: String
 
     @Bean
     fun polemicaWebClient(): PolemicaClient {
         return PolemicaClientImpl(
-            WebClient.builder()
-                .baseUrl(botBaseUrl)
-                .defaultHeader("Authorization", "Bearer $botToken")
-                .build()
+            polemicaBaseUrl,
+            polemicaUsername,
+            polemicaPassword
         )
     }
 }
